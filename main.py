216b943c51008dc76170a9b3bc41917fb32dc3e6
Arthur Correnson <arthur.correnson@gmail.com>
2019-08-22 10:06:41 +0200

    remove useless functions

--- main.py
+++ main.py
@@ -7,18 +7,6 @@ from discord.ext import commands
 
 cote = 0
 
-def set_cote(txt):
-	"""
-		Determines the new kebab cote.
-	"""
-	
-	# test on txt here
-	# ...
-	
-	global cote
-	return cote + 1
-
-
 # =========== bot functions =============
 
 bot = commands.Bot(command_prefix='?')
@@ -26,29 +14,30 @@ bot = commands.Bot(command_prefix='?')
 
 @bot.event
 async def on_ready():
-	game = discord.Game("Surveiller Tristan")
-	# affiche le statut
-	await bot.change_presence(status=discord.Status.online, activity=game)
-	print('*Bot is ready*')
+    game = discord.Game("Surveiller Tristan")
+    # affiche le statut
+    await bot.change_presence(status=discord.Status.online, activity=game)
+    print('*Bot is ready*')
 
 
 @bot.event
 async def on_message(msg):
-	"""
-		Process messages and update the Kebab cote>
-	"""
+    """
+        Process messages and update the Kebab cote>
+    """
 
-	global cote
-	
-	# debug
-	print(msg.author)
-	
-	# update the cote here
-	# ...
+    global cote
+    
+    # debug
+    if msg.author == '0x5AD':
+        print(msg.content)
+    
+    # update the cote here
+    # ...
 
-	# since on_message overwrite commands
-	# we need to manually process them 
-	await bot.process_commands(msg)
+    # since on_message overwrite commands
+    # we need to manually process them
+    await bot.process_commands(msg)
 
 
 @bot.command()
@@ -58,17 +47,49 @@ async def ping(ctx):
 
 @bot.command()
 async def kebab(ctx):
+    """
+        A command to display the kebab cote.
+    """
+    global cote
+
+    msg = "```"
+    msg += "La cote kebab de Tristan est de "
+    msg += str(cote)
+    msg += "```"
+    await ctx.send(msg)
+
+
+@bot.command()
+async def up(ctx):
+    """
+        A command to update the kebab cote.
+    """
+
+    global cote
+
+    cote += 1
+    msg = "```"
+    msg += "+1 pour Tristan : ta cote est maintenant de "
+    msg += str(cote)
+    msg += "```"
+    await ctx.send(msg)
+
+
+@bot.command()
+async def down(ctx):
 	"""
-		A command to display the kebab cote.
+		A command to decrement the kebab cote.
 	"""
+	
 	global cote
 
-	msg = "```"
-	msg += "La cote kebab de Tristan est de "
-	msg += str(cote)
-	msg += "```"
-	print('debug')
-	await ctx.send(msg)
+    cote -= 1
+    msg = "```"
+    msg += "-1 pour Tristan : ta cote est maintenant de "
+    msg += str(cote)
+    msg += "```"
+    await ctx.send(msg)
+
 
 
 bot.run(os.environ["token"])
